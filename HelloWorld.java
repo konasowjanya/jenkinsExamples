9258daed1912ec18f212faa726a0904ffb661d33
unknown <user34@ALCHEMYCLOUD.CO.IN>
2020-07-02 11:40:53 +0530

    Add Hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("For loopstatrtin");
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
